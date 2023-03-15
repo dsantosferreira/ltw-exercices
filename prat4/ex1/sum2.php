686e454c9d0fa6826788c04bf8868d66e92341fa
dsantosferreira <unidsferreira2003@gmail.com>
2023-03-15 11:46:51 +0000

    Finished php setup and exercice 1

---
+++ prat4/ex1/sum2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1>
+            <?=$_GET['num1'] + $_GET['num2']?> 
+        </h1>
+    </body>
+    <a href="form2.html">Do another sum!</a>
+</html>
+
